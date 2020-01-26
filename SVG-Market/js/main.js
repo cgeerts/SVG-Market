d339bde5d7e3629595859bd022335a4c8c5ea244
cgeerts <55294018+cgeerts@users.noreply.github.com>
2020-01-25 23:49:37 -0500

    Add files via upload

---
+++ SVG-Market/js/main.js
@@ -0,0 +1,40 @@
+//// this is a self-invoking anonymous function (IIFE/module)
+
+(() => {
+  // print("some message");
+  console.log("Hello from JavaScript");
+
+  // find the button element on the page by its ID with JavaScript
+  let theButton = document.querySelector("#myButton"),
+      theHeading = document.querySelector('h1'),
+      svgImage = document.querySelector("#svg");
+
+  // changes the heading text
+  function changeText() {
+    theHeading.textContent = "Hope you enjoy! Come again soon!";
+  }
+
+  // log the id of the svg to the console when a user hovers on it
+  function logSVG() {
+    console.log(this.id);
+  }
+
+  theButton.addEventListener("click", changeText);
+  svgImage.addEventListener("mouseover", logSVG);
+  //document.querySelector('h1').textContent = "Hello from the console";
+
+   const    svgImg1 = document.querySelector('#svgImg1'),
+            svgImg2 = document.querySelector('#svgImg2'),
+            svgImg3 = document.querySelector('#svgImg3'),
+            svgImg4 = document.querySelector('#svgImg4'),
+            svgImg5 = document.querySelector('#svgImg5'),
+            svgImg6 = document.querySelector('#svgImg6'),
+            svgImg7 = document.querySelector('#svgImg7'),
+            svgImg8 = document.querySelector('#svgImg8'),
+            svgImg9 = document.querySelector('#svgImg9'),
+            svgImg10 = document.querySelector('#svgImg10');
+
+    function logoSVG() {
+        this.classList.toggle("selected");
+    }
+})();
